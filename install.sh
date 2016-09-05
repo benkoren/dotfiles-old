52e71fe5dae52e92973f5a22a0d1832ac85fcf1a
Ben Koren <benkoren@gmail.com>
2016-09-04 21:38:15 -0500

    Added vim-plug installer

--- install.sh
+++ install.sh
@@ -10,3 +10,7 @@ ln -s "${SCRIPTPATH}/gitconfig" ~/.gitconfig
 ln -s "${SCRIPTPATH}/tmux.conf" ~/.tmux.conf
 
 
+# Install vim-plug for neovim
+curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
